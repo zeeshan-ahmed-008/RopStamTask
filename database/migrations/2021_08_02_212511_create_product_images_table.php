86589c028e961b35cf652403b2a1b9bfee5dceb5
zeeshan-ahmed-008 <z.itraders93@gmail.com>
2021-08-03 08:02:32 +0500

    Done

--- database/migrations/2021_08_02_212511_create_product_images_table.php
+++ database/migrations/2021_08_02_212511_create_product_images_table.php
@@ -15,6 +15,8 @@ class CreateProductImagesTable extends Migration
     {
         Schema::create('product_images', function (Blueprint $table) {
             $table->id();
+            $table->string('image');            
+            $table->integer('product_id');            
             $table->timestamps();
         });
     }
--- database/migrations/2021_08_02_212533_create_products_table.php
+++ database/migrations/2021_08_02_212533_create_products_table.php
@@ -15,6 +15,19 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('image');
+            $table->string('name');
+            $table->integer('category_id');
+            $table->enum('currency', ['$', '₹'])->default('₹');
+            $table->integer('price')->default(0);
+            $table->enum('rent_type', ['Month', 'Year'])->default('Month');
+            $table->float('average_ratings', 8, 1)->default(0);
+            $table->integer('total_ratings')->default(0);
+            $table->integer('total_reviews')->default(0);
+            $table->integer('refundable')->default(0);
+            $table->string('coupon_code')->nullable();            
+            $table->enum('size', ['6x3', '6x4', '6x5', '6x6'])->default('6x3');
+            $table->string('tenure')->nullable();
             $table->timestamps();
         });
     }
